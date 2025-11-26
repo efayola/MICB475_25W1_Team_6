6ef4be5f9afe30ff576f84c9a40d5cf6998e446e
zhulouis <louiszhu0210@gmail.com>
2025-11-26 10:00:08 -0800

    Update DEG heatmaps script

--- R_project/calf_DEG/calf_DEG_heatmaps.R
+++ R_project/calf_DEG/calf_DEG_heatmaps.R
@@ -3,4 +3,13 @@ library(phyloseq)
 library(DESeq2)
 
 #load data
-load("calf_phyloseq_rare.RData")
+load("all_phyloseq_objects/calf_phyloseq_rare.RData")
+
+#### DESeq ####
+calf_plus1 <- transform_sample_counts(calf_phyloseq_rare, function(x) x+1)
+calf_timepoints_deseq <- phyloseq_to_deseq2(calf_plus1, ~`host_age`)
+DESEQ_calf_timepoints <- DESeq(calf_timepoints_deseq)
+res_calf_timepoints <- results(DESEQ_calf_timepoints, 
+                               name = "host_age_vs_age", 
+                               tidy = TRUE)
+View(res)
