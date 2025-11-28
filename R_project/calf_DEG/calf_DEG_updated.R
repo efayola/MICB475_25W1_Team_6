78018ad18561e2cc57ac4bc79983e989df3a3989
zhulouis <louiszhu0210@gmail.com>
2025-11-28 00:30:24 -0800

    Updated Script for Calf DEG volcano w/ label

--- R_project/calf_DEG/calf_DEG_updated.R
+++ R_project/calf_DEG/calf_DEG_updated.R
@@ -8,6 +8,7 @@ library(rstatix)
 library(vegan)
 library(ggplot2)
 library(patchwork)
+library(ggrepel)
 
 set.seed(42)
 #### Load data ####
@@ -40,9 +41,39 @@ resultsNames(DESEQ_calf_sex)
 res_calf_sex <- results(DESEQ_calf_sex, name = "host_sex_male_vs_female", tidy = TRUE)
 View(res_calf_sex)
 
+# To get table of results
+sigASVs_sex <- res_calf_sex %>% 
+  filter(padj<0.01 & abs(log2FoldChange)>1.9) %>%
+  dplyr::rename(ASV=row)
+View(sigASVs_sex)
+#To get ASV
+sigASVs_sex_vec <- sigASVs_sex %>%
+  pull(ASV)
+
+# Prune phyloseq file
+male_female_DESeq <- prune_taxa(sigASVs_sex_vec,calf_phyloseq_no_diet_and_T9)
+sigASVs_sex <- tax_table(male_female_DESeq) %>% as.data.frame() %>%
+  rownames_to_column(var="ASV") %>%
+  right_join(sigASVs_sex) %>%
+  arrange(log2FoldChange) %>%
+  mutate(Genus = make.unique(Genus)) %>%
+  mutate(Genus = factor(Genus, levels=unique(Genus)))
+View(sigASVs_sex)
+
+res_calf_sex_labeled <- res_calf_sex %>%
+  left_join(
+    sigASVs_sex %>% select(ASV, Genus), # Select only the ID and the label column
+    by = c("row" = "ASV") # Join 'row' from res_calf_sex to 'ASV' from sigASVs_sex
+  )
+View(res_calf_sex_labeled)
+
+#Create a filtered dataframe containing only the points you want to label
+label_data <- res_calf_sex_labeled %>%
+  filter(!is.na(Genus)) # Keep only the rows that got a match (i.e., have a Genus label)
+View(label_data)
 
 #graph volcano plot
-vol_plot_sex_upd <- res_calf_sex %>%
+vol_plot_sex_upd <- res_calf_sex_labeled %>%
   mutate(significant = padj<0.01 & abs(log2FoldChange)>1.9) %>%
   ggplot() +
     geom_point(aes(x=log2FoldChange, y=-log10(padj), col=significant),size = 2, alpha = 0.8) + 
@@ -51,6 +82,14 @@ vol_plot_sex_upd <- res_calf_sex %>%
       labels = c("Not significant", "Significant"),
       name   = "Significance",
     ) + 
+    geom_text_repel(
+      data = sigASVs_sex, # Use the filtered data for only the points you want to label
+      aes(x = log2FoldChange, y = -log10(padj), label = Genus),
+      size = 3.5,
+      box.padding = 0.5,
+      point.padding = 0.5,
+      max.overlaps = Inf # Use this cautiously for many points, but ensures all labels are attempted
+    ) +
     geom_vline(xintercept = c(-1.9, 1.9), linetype = "dashed", color = "darkgrey") +
     geom_hline(yintercept = -log10(0.01), linetype = "dashed", color = "darkgrey") +
     # labs(title = title,
