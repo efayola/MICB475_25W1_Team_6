71cf723b41da0e8e1297cf268038bfecb503e6e6
zhulouis <louiszhu0210@gmail.com>
2025-11-28 17:16:50 -0800

    Updated Tax Bar plot script

--- R_project/tax_bar_plot/tax_bar_plot_script.R
+++ R_project/tax_bar_plot/tax_bar_plot_script.R
@@ -10,19 +10,32 @@ library(ggplot2)
 library(patchwork)
 library(ggrepel)
 
-load("all_phyloseq_objects/calf_phyloseq_rare.RData")
+#### Load Data ####
+# load("all_phyloseq_objects/calf_phyloseq_rare.RData")
+# rank_names(calf_phyloseq_rare)
+# 
+# #merges species that have the same taxonomy at genus level
+# calf_phyloseq_rare_phylum <- tax_glom(calf_phyloseq_rare, taxrank = "Phylum")
+# save(calf_phyloseq_rare_phylum, file="all_phyloseq_objects/calf_phyloseq_rare_phylum.RData")
 
-# Plot bar plot of taxonomy
-plot_bar(calf_phyloseq_rare, fill="Phylum") 
+load("all_phyloseq_objects/calf_phyloseq_rare_phylum.RData")
+nsamples(calf_phyloseq_rare_phylum) #480 removed 3 by rarefractino
+
+calf_phyloseq_no_diet_and_T9 <- subset_samples(calf_phyloseq_rare_phylum, host_age != "not applicable" & host_age !="T9")
+nsamples(calf_phyloseq_no_diet_and_T9)
 
 # Convert to relative abundance
-calf_RA <- transform_sample_counts(calf_phyloseq_rare, function(x) x/sum(x))
+calf_RA <- transform_sample_counts(calf_phyloseq_no_diet_and_T9, function(x) x/sum(x))
 
-# To remove black bars, "glom" by phylum first
-calf_phylum <- tax_glom(calf_RA, taxrank = "Phylum", NArm=FALSE)
+# Code to glom later by families
+# # To remove black bars, "glom" by phylum first
+# calf_phylum <- tax_glom(calf_RA, taxrank = "Phylum", NArm=FALSE)
 
-gg_taxa <- plot_bar(calf_phylum, fill="Phylum") + 
-  facet_wrap(.~subject, scales = "free_x")
+#Aggregate relative abundances by Phylum
+calf_RA_top10 <- prune_taxa(taxa_sums(calf_RA), calf_RA)
+
+gg_taxa <- plot_bar(top10_phyla, fill="Phylum") + 
+  facet_wrap(.~host_age, scales = "free_x")
 gg_taxa
 
 ggsave("plot_taxonomy.png"
