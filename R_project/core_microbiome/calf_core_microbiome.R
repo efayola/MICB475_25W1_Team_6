2ab06f610c6ad3007c8a7a1e35ef866fa99fed1d
Leanne Su <leannesu@Leannes-MacBook-Air.local>
2025-12-11 16:42:47 -0800

    updated core microbiome - tax glom to genus and changed to T1,T6,T8

--- R_project/core_microbiome/calf_core_microbiome.R
+++ R_project/core_microbiome/calf_core_microbiome.R
@@ -5,38 +5,42 @@ library(microbiome)
 library(ggVennDiagram)
 
 #### Load data ####
-load("all_phyloseq_objects/calf_phyloseq_final.RData")
+load("all_phyloseq_objects/calf_phyloseq_final_genus.RData")
+
+# replace taxa names with genus names
+genus_names <- make.unique(as.character(tax_table(calf_phyloseq_final_genus)[,"Genus"]))
+taxa_names(calf_phyloseq_final_genus) <- genus_names
 
 #### "core" microbiome ####
 
 # Convert to relative abundance
-calf_RA <- transform_sample_counts(calf_phyloseq_final, fun=function(x) x/sum(x))
+calf_RA <- transform_sample_counts(calf_phyloseq_final_genus, fun=function(x) x/sum(x))
 
 
-### T1 and T5 ###
-# subset by t1 and t5
+### T1 and T6 ###
+# subset by t1 and t6
 calf_T1 <- subset_samples(calf_RA, `host_age` == "T1")
-calf_T5 <- subset_samples(calf_RA, `host_age` == "T5")
+calf_T6 <- subset_samples(calf_RA, `host_age` == "T6")
 
 # further subset by sex
 calf_T1_female <- subset_samples(calf_T1, host_sex == "female")
 calf_T1_male <- subset_samples(calf_T1, host_sex == "male")
 
-calf_T5_female <- subset_samples(calf_T5, host_sex == "female")
-calf_T5_male <- subset_samples(calf_T5, host_sex == "male")
+calf_T6_female <- subset_samples(calf_T6, host_sex == "female")
+calf_T6_male <- subset_samples(calf_T6, host_sex == "male")
 
 # extract core taxa
 core_T1_female <- core_members(calf_T1_female, detection = 0.001, prevalence = 0.3)
 core_T1_male <- core_members(calf_T1_male, detection = 0.001, prevalence = 0.3)
 
-core_T5_female <- core_members(calf_T5_female, detection = 0.001, prevalence = 0.3)
-core_T5_male <- core_members(calf_T5_male, detection = 0.001, prevalence = 0.3)
+core_T6_female <- core_members(calf_T6_female, detection = 0.001, prevalence = 0.3)
+core_T6_male <- core_members(calf_T6_male, detection = 0.001, prevalence = 0.3)
 
 # make 4-way Venn diagram of core taxa overlap across sex and T1/T5 
-list_T1_T5_sex <- list("T1 Female" = core_T1_female, "T1 Male" = core_T1_male,
-                 "T5 Female" = core_T5_female, "T5 Male" = core_T5_male)
+list_T1_T6_sex <- list("T1 Female" = core_T1_female, "T1 Male" = core_T1_male,
+                 "T6 Female" = core_T6_female, "T6 Male" = core_T6_male)
 
-T1_T5_sex_venn <- ggVennDiagram(x = list_T1_T5_sex, 
+T1_T6_sex_venn <- ggVennDiagram(x = list_T1_T6_sex, 
                                 label_alpha = 0, 
                                 edge_size = 0.8,     
                                 set_size = 5,         
@@ -47,33 +51,33 @@ T1_T5_sex_venn <- ggVennDiagram(x = list_T1_T5_sex,
                                       barwidth  = unit(1, "cm")))
 
 # save Venn diagram
-ggsave("venn_sex_T1_T5.png", T1_T5_sex_venn, width = 10, height = 10, dpi = 300)
+ggsave("venn_sex_T1_T6.png", T1_T6_sex_venn, width = 10, height = 10, dpi = 300)
 
 
-### T5 and T8 ###
-# subset by T5 and T8
-calf_T5 <- subset_samples(calf_RA, `host_age` == "T5")
+### T6 and T8 ###
+# subset by T6 and T8
+calf_T6 <- subset_samples(calf_RA, `host_age` == "T6")
 calf_T8 <- subset_samples(calf_RA, `host_age` == "T8")
 
 # further subset by sex
-calf_T5_female <- subset_samples(calf_T5, host_sex == "female")
-calf_T5_male <- subset_samples(calf_T5, host_sex == "male")
+calf_T6_female <- subset_samples(calf_T6, host_sex == "female")
+calf_T6_male <- subset_samples(calf_T6, host_sex == "male")
 
 calf_T8_female <- subset_samples(calf_T8, host_sex == "female")
 calf_T8_male <- subset_samples(calf_T8, host_sex == "male")
 
 # extract core taxa
-core_T5_female <- core_members(calf_T5_female, detection = 0.001, prevalence = 0.3)
-core_T5_male <- core_members(calf_T5_male, detection = 0.001, prevalence = 0.3)
+core_T6_female <- core_members(calf_T6_female, detection = 0.001, prevalence = 0.3)
+core_T6_male <- core_members(calf_T6_male, detection = 0.001, prevalence = 0.3)
 
 core_T8_female <- core_members(calf_T8_female, detection = 0.001, prevalence = 0.3)
 core_T8_male <- core_members(calf_T8_male, detection = 0.001, prevalence = 0.3)
 
 # make 4-way Venn diagram of core taxa overlap across sex and T5/T8 
-list_T5_T8_sex <- list("T5 Female" = core_T5_female, "T5 Male" = core_T5_male,
+list_T6_T8_sex <- list("T6 Female" = core_T6_female, "T6 Male" = core_T6_male,
                        "T8 Female" = core_T8_female, "T8 Male" = core_T8_male)
 
-T5_T8_sex_venn <- ggVennDiagram(x = list_T5_T8_sex, 
+T6_T8_sex_venn <- ggVennDiagram(x = list_T6_T8_sex, 
                                 label_alpha = 0, 
                                 edge_size = 0.8,     
                                 set_size = 5,         
@@ -84,7 +88,7 @@ T5_T8_sex_venn <- ggVennDiagram(x = list_T5_T8_sex,
                                       barwidth  = unit(1, "cm")))
 
 # save Venn diagram
-ggsave("venn_sex_T5_T8.png", T5_T8_sex_venn, width = 10, height = 10, dpi = 300)
+ggsave("venn_sex_T6_T8.png", T6_T8_sex_venn, width = 10, height = 10, dpi = 300)
 
 
 ### T7 and T8 ###
